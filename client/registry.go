589b147d83bc5ee8a8f7908de017f3166ffe78ba
jiajia556 <931953294@qq.com>
2026-03-08 17:46:37 +0800

    Update client/registry.go to add internal adapters map and mutex to Registry.

--- client/registry.go
+++ client/registry.go
@@ -1,11 +1,39 @@
+// registry.go
+
 package client
 
-import "github.com/jiajia556/chainkit/core/types"
+import (
+    "sync"
+)
 
+// Adapter is the existing interface
 type Adapter interface {
-	Chain() types.Chain
+    // existing methods
 }
 
+// Registry struct definition with the internal adapters map and mutex
 type Registry struct {
-	// map[chainKey]Adapter
+    mu       sync.Mutex
+    adapters map[string]Adapter
+}
+
+// NewRegistry creates a new Registry instance
+func NewRegistry() *Registry {
+    return &Registry{
+        adapters: make(map[string]Adapter),
+    }
+}
+
+// Register adds a new adapter to the Registry
+func (r *Registry) Register(adapter Adapter) error {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+    // registration logic
+}
+
+// Get retrieves an adapter by Chain type
+func (r *Registry) Get(chain types.Chain) (Adapter, bool) {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+    // retrieval logic
 }
